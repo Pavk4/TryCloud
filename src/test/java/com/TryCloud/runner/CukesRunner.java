9ddd5bade99bad3e62cd84cdca80d67717610162
WilliamSegura <wasegura@udel.edugit config --global user.emailgit config --global user.email wasegura@udel.edugit config --global user.email wasegura@udel.edu>
2022-11-16 09:44:10 -0500

    TryCLoud Project Created

--- src/test/java/com/TryCloud/runner/CukesRunner.java
+++ src/test/java/com/TryCloud/runner/CukesRunner.java
@@ -6,7 +6,11 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = "html:target/cucumber-report.html",
+        plugin = {"html:target/cucumber-report.html",
+                    "json:target/cucumber-report.json",
+                "junit:target/junit/junit-report.xml",
+                "rerun:target/rerun.txt"
+        },
         features = "src/test/resources/features" ,
         glue = "com/TryCloud/step_definition" ,
         dryRun = false,
