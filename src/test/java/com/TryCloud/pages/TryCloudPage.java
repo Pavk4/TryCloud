ec5c15fb5d1b560f82c5c9c6e3baeaed326280ec
pavlin <goranov.pav@gmail.com>
2022-11-15 23:35:18 -0500

    US 7 completed

--- src/test/java/com/TryCloud/pages/TryCloudPage.java
+++ src/test/java/com/TryCloud/pages/TryCloudPage.java
@@ -8,10 +8,46 @@ import org.openqa.selenium.support.PageFactory;
 
 public class TryCloudPage {
     public TryCloudPage() {
-    PageFactory.initElements(Driver.getDriver(),this);
+        PageFactory.initElements(Driver.getDriver(), this);
     }
-    @FindBy (className = "button new")
+
+    @FindBy(xpath = "//input[@id='user']")
+    public WebElement usernameInput;
+
+    @FindBy(xpath = "//input[@id='password']")
+    public WebElement passwordInput;
+
+    @FindBy(xpath = "//input[@id='submit-form']")
+    public WebElement loginBtn;
+
+    @FindBy(xpath = "(//li[@data-id='files']/a)[1]")
+    public WebElement userIsOnTheFilesModule;
+
+    @FindBy(css = "a[class='button new']")
     public WebElement addButton;
 
+    @FindBy(xpath = "(//span[@class='displayname'])[2]")
+    public WebElement newFolderButton;
+
+    @FindBy(css = "input[id='view13-input-folder']")
+    public WebElement newFolderInput;
+
+    @FindBy(xpath = "//input[@id='view13-input-folder']")
+    public WebElement folderInputRename;
+
+    @FindBy(xpath = "//input[@class='icon-confirm']")
+    public WebElement confirmButton;
+
+
+    public void login(String username, String password) {
+        usernameInput.sendKeys(username);
+        passwordInput.sendKeys(password);
+        loginBtn.click();
+        // userIsOnTheFilesModule.click();
+        //addButton.click();
+        //newFolderButton.click();
+
+
+    }
 }
 
--- src/test/java/com/TryCloud/step_definition/MyStepDefs.java
+++ src/test/java/com/TryCloud/step_definition/MyStepDefs.java
@@ -1,33 +1,48 @@
 package com.TryCloud.step_definition;
 
+import com.TryCloud.pages.TryCloudPage;
+import com.TryCloud.utilities.ConfigurationReader;
+import com.TryCloud.utilities.Driver;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class MyStepDefs {
+
+    TryCloudPage tryCloudPage = new TryCloudPage();
+
+    @Before
+    public void user_on_the_dashboard_page() {
+        Driver.getDriver().get(ConfigurationReader.getProperty("env"));
+        tryCloudPage.login(ConfigurationReader.getProperty("username1"), ConfigurationReader.getProperty("password"));
+    }
+
+
     @Given("user is on the Files module")
     public void userIsOnTheFilesModule() {
-        System.out.println("user is on the Files module");
+        tryCloudPage.userIsOnTheFilesModule.click();
+
     }
 
     @When("the user selects add icon")
     public void theUserSelectsAddIcon() {
-        System.out.println("the user selects add icon");
+        tryCloudPage.addButton.click();
     }
 
     @And("user clicks in New folder option")
     public void userClicksInNewFolderOption() {
-        System.out.println("user clicks in New folder option");
+        tryCloudPage.newFolderButton.click();
     }
 
-    @And("user creates New folder")
+    @Then("user creates New folder")
     public void userCreatesNewFolder() {
-        System.out.println("user creates New folder");
+        tryCloudPage.newFolderInput.click();
+        tryCloudPage.folderInputRename.sendKeys("Pavlin folder");
+        tryCloudPage.confirmButton.click();
+
     }
 
-    @Then("user can upload a file")
-    public void userCanUploadAFile() {
-        System.out.println("user can upload a file");
-    }
+
 }
